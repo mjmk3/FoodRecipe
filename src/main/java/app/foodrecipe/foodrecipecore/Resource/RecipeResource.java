ce10a21c173ae341a8f7e6df66f6d095a746ecd1
Mohamad J Makki <mohamad.j.makki@gmail.com>
2023-12-22 16:28:02 +0300

    Resources

--- src/main/java/app/foodrecipe/foodrecipecore/Resource/RecipeResource.java
+++ src/main/java/app/foodrecipe/foodrecipecore/Resource/RecipeResource.java
@@ -1,4 +1,22 @@
 package app.foodrecipe.foodrecipecore.Resource;
 
+import app.foodrecipe.foodrecipecore.Service.RecipeService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recipe")
+@CrossOrigin(origins = "*")
+@Tag(name = "Recipes")
 public class RecipeResource {
+
+    private final RecipeService recipeService;
+
+    @Autowired
+    public RecipeResource(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 }
--- src/main/java/app/foodrecipe/foodrecipecore/Resource/UserResource.java
+++ src/main/java/app/foodrecipe/foodrecipecore/Resource/UserResource.java
@@ -1,6 +1,8 @@
 package app.foodrecipe.foodrecipecore.Resource;
 
+import app.foodrecipe.foodrecipecore.Service.UserService;
 import io.swagger.v3.oas.annotations.tags.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,4 +10,11 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*")
 @Tag(name = "Users")
 public class UserResource {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
 }
