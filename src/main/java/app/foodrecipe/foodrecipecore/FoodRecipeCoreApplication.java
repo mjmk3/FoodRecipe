de1f09d209ce67e15f89d4e8768bbb7ebb68bdc2
Mohamad J Makki <mohamad.j.makki@gmail.com>
2023-12-22 15:02:28 +0300

    Spring Recipe

---
+++ src/main/java/app/foodrecipe/foodrecipecore/FoodRecipeCoreApplication.java
@@ -0,0 +1,13 @@
+package app.foodrecipe.foodrecipecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecipeCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodRecipeCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/foodrecipe/foodrecipecore/FoodRecipeCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package app.foodrecipe.foodrecipecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecipeCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
