0f8bfe8a6c42a5455304bfd23b79c7bc06912cba
Mohamad J Makki <mohamad.j.makki@gmail.com>
2023-12-25 02:06:40 +0300

    Tailwnidcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./comp/layout/header/header.component";
+import {FooterComponent} from "./comp/layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
