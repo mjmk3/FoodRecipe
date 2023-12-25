92895354c8839ce5e6b01a501bcf271daa9adf40
Mohamad J Makki <mohamad.j.makki@gmail.com>
2023-12-25 06:33:19 +0300

    Tailwnidcss

--- src/app/comp/access/auth/auth.component.ts
+++ src/app/comp/access/auth/auth.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {NgClass} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
   selector: 'app-auth',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
   isRegister= false;
+  togglePanel(){
+    this.isRegister=!this.isRegister
+  }
 }
