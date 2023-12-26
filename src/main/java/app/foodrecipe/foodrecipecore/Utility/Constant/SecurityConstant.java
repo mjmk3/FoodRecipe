d49b5f9ef0555ea02c7e4693c146b5adda0591c9
Mohamad J Makki <mohamad.j.makki@gmail.com>
2023-12-26 12:20:44 +0300

    Resources

---
+++ src/main/java/app/foodrecipe/foodrecipecore/Utility/Constant/SecurityConstant.java
@@ -0,0 +1,4 @@
+package app.foodrecipe.foodrecipecore.Utility.Constant;
+
+public class SecurityConstant {
+}
---
+++ src/main/java/app/foodrecipe/foodrecipecore/Utility/Payload/HttpResponse.java
@@ -0,0 +1,4 @@
+package app.foodrecipe.foodrecipecore.Utility.Payload;
+
+public class HttpResponse {
+}
